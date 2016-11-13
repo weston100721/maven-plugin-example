708f86b559ac4d9c3f88131c4f45b9af8cbae487
weston <weston_contribute@163.com>
2016-11-13 21:36:01 +0800

    add maven-assembly-plugin module

---
+++ maven-assembly-plugin/src/main/java/com/zhaoliang/maven/assembly/plugin/Test.java
@@ -0,0 +1,19 @@
+package com.zhaoliang.maven.assembly.plugin;
+
+/**
+ * Created by zhaoliang (weston_contribute@163.com) on 2016/11/13.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        String config = "hello.properties";
+        System.out.println(config);
+        test1();
+    }
+
+    private static void test1() {
+        System.out.println("test1");
+    }
+
+}
